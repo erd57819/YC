dda9c5f10964e40b64e771799872d79263e1deb6
김태영 <erd57819@gmail.com>
2025-05-23 17:20:47 +0900

    account serailizer db fixed

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,5 +1,4 @@
-# backend/accounts/serializers.py
-
+from django.db import transaction
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from dj_rest_auth.registration.serializers import RegisterSerializer
@@ -20,10 +19,22 @@ class CustomRegisterSerializer(RegisterSerializer):
         data['salary'] = self.validated_data.get('salary', None)
         data['wealth'] = self.validated_data.get('wealth', None)
         return data
+    
+    @transaction.atomic
+    def save(self, request):
+        user = super().save(request)
+        
+        user.nickname = self.validated_data.get('nickname', None)
+        user.age = self.validated_data.get('age', None)
+        user.salary = self.validated_data.get('salary', None)
+        user.wealth = self.validated_data.get('wealth', None)
+        
+        user.save()
+        
+        return user
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'nickname', 'age', 'salary', 'wealth', 'created_at', 'updated_at') # 'user_id' -> 'id'
         read_only_fields = ('username', 'created_at', 'updated_at')
-# ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
